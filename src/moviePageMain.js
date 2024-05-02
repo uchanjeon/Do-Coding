1a07a983148af9514831fe7ceeb9245ee5a4971d
이강산 <gangsan1414@gmail.com>
2024-05-02 20:04:24 +0900

    파일 이름 수정

---
+++ src/moviePageMain.js
@@ -0,0 +1,8 @@
+// import {createMovieDetail} from "./createMovieDetail.js"
+// // import {} from "./review.js";
+
+// window.onload = () {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const movieId = urlParams.get("id");
+//     creatMovieDetail(movieId);
+// };
