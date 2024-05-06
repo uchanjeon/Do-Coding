28ab8b126719f12e39b21fe5d81b4078b9a69db1
이강산 <gangsan1414@gmail.com>
2024-05-06 15:37:25 +0900

    검색기능 재활성화(enter포함), test.js삭제

--- src/homeMain.js
+++ src/homeMain.js
@@ -1,14 +1,23 @@
 import { fetchMovieData } from "./nowPlayingAPI.js"; //1. API를 가져온다.
 import { createMovieCards } from "./createMovieCards.js"; //2. 영화 카드를 생성한다.
-// import { searchMovieCards } from "./searchMovieCards.js";
+import { searchMovieCards, pressEnter } from "./searchMovieCards.js";
 
 // import { searchMovies } from "./search.js"; // 4.
 // 1. API를 가져온다.
-const movies = await fetchMovieData(); //async
+export const movies = await fetchMovieData(); //async
 
 // 2. 영화 카드를 생성한다. 3. 영화 카드에 함수를 부여한다.
 createMovieCards(movies);
 
-// 4. 영화 상단 keyword 검색기능을 부여한다.
-// input과 동일하지 않으면 사라짐. 그리고 다시 영화 카드에 함수를 부여.
-// enter키도 가능한거 여기다 넣자.
+// 4. 검색기능 : 엔터키안됨
+const searchInput = document.querySelector("#searchInput");
+searchInput.focus();
+//검색버튼누르면 검색 실행
+document.querySelector("#searchBtn").addEventListener("click", (event) => {
+  event.preventDefault();
+  searchMovieCards(searchInput.value);
+});
+//엔터누르면 검색 실행
+document
+  .getElementById("searchInput")
+  .addEventListener("keyup", (event) => pressEnter(event));
