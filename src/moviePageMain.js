8019cd1aa4d953b4ea098572a5c1142368b9186c
SuhyeonJin <lofi0613@gmail.com>
2024-05-07 18:43:06 +0900

    상세정보창수정완료

--- src/moviePageMain.js
+++ src/moviePageMain.js
@@ -14,22 +14,19 @@ document.addEventListener("DOMContentLoaded", function () {
       );
 
       if (clickedMovie) {
-        const temp_html = `
-        <div class="movieInfo">
-          <div class="imagesection">
-            <img class="image" src="https://image.tmdb.org/t/p/w500${clickedMovie.poster_path}" alt="..." />
-          </div>
-          <div class="infotext">
-            <div class="card-body">
-              <h3 class="card-title">${clickedMovie.title}</h3>
-              <p class="card-text">${clickedMovie.release_date}</p>
-              <p class="card-text"> 평점 : ${clickedMovie.vote_average}</p>
-              <p class="card-text">${clickedMovie.overview}</p>
-            </div>
-          </div>
-        </div>
-      `;
-        document.getElementById("bigbox1").innerHTML = temp_html;
+        document.querySelector("#imagesection").innerHTML = `
+        <img class="image" src="https://image.tmdb.org/t/p/w500${clickedMovie.poster_path}" alt="..." />`;
+
+        document.querySelector("#cardtitle").innerHTML = `
+        ${clickedMovie.title}`;
+
+        document.querySelector("#innertext").innerHTML = `
+                <p class="card-text">개봉일 : ${clickedMovie.release_date}</p>
+                <p class="card-text"> 평점 : ${clickedMovie.vote_average}</p>
+                <br>
+                <p class="card-text"> 상세정보</p>
+                <p> ${clickedMovie.overview}</p>`;
+
       } else {
         console.error("Clicked movie not found in the movie list");
       }
@@ -39,38 +36,3 @@ document.addEventListener("DOMContentLoaded", function () {
       a;
     });
 });
-
-
-//추가기능 - 출연진 / 예매하기 기능 다른 api에서 받아오기
-async function fetchMovieData() {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZmFmYTFlODI1OGYwZDc3MWEwMzRjOWM3OTNiNjgzMCIsInN1YiI6IjY2MjZmZDQ4MTc2YTk0MDE3ZjgxMmVjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NKjXh9Bx4UxPLwQzr6nBDrLrSEAQm89Mqcv8XlKgXms",
-    },
-  };
-
-  const clickedMovieId = localStorage.getItem("clickedidmovie");
-  let address = `https://api.themoviedb.org/3/movie/${clickedMovieId}?append_to_response=credits&language=en-en`
-
-  fetch(address, options)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      moveToReserve(data);
-
-
-
-    })
-}
-
-let moveToReserve = (data) => {
-  document.querySelector("#reserve").addEventListener('click', () => {
-    location.href=data['homepage'];
-  });
-}
-
-fetchMovieData();
-
