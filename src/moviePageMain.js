7e62e592e6caef7e52d38bf406cb27fe396ab889
이강산 <gangsan1414@gmail.com>
2024-05-08 18:25:46 +0900

    otherInfo.js를 Import하게 했습니다.

--- src/moviePageMain.js
+++ src/moviePageMain.js
@@ -1,3 +1,6 @@
-import { showMovieInfo } from "./showMovieInfo.js"; //
+import { showMovieInfo } from "./showMovieInfo.js";
+import { movieActors, movieWatch } from "./otherInfos.js";
 
-showMovieInfo();
+showMovieInfo(); // 영화 정보
+movieActors(); // 출연진
+movieWatch(); // 보러가기
